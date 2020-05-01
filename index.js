7b054f47fa14e46adfc9a56661d3e740768c4c0d
@maxgfr <maxgolfier@gmail.com>
2020-05-01 09:49:10 +0200

    push

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const youtubedl = require('youtube-dl')
 const fs = require('fs');
+const request = require('request');
 const readline = require("readline");
+const youtubedl = require('youtube-dl');
 
-var url = ''
+var url = '';
+var dl = false;
 
 /*
 process.argv.forEach(function (val, index, array) {
@@ -15,17 +17,34 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question("URL of your youtube video : ", function(selected_url) {
+rl.question("URL of the media : ", function(selected_url) {
     url = selected_url;
-    rl.close();
+    rl.question("Do you want to download the file ? (yes or no) : ", function(selected_dl) {
+      selected_dl = selected_dl.toLowerCase();
+      if(selected_dl == 'yes' || selected_dl == 'y') dl = true;
+        rl.close();
+    });
 });
 
 rl.on("close", function() {
   youtubedl.getInfo(url, [], async (err, info) => {
     if (err) console.log(err)
+    console.log('id:', info.id)
+    console.log('title:', info.title)
+    console.log('url:', info.url)
+    console.log('thumbnail:', info.thumbnail)
+    console.log('description:', info.description)
+    console.log('filename:', info._filename)
+    console.log('format id:', info.format_id)
+    if(dl) {
+      fs.mkdir('./download', { recursive: true }, (err) => {
+        if (err) throw err;
+        request(info.url).pipe(fs.createWriteStream('./download/'+info._filename))
+      });
+    }
     fs.writeFile('result.json', JSON.stringify(info, null, 2), function (err) {
-      if (err) console.log(err);
+      if (err) throw err;
     });
   })
-    //process.exit(0);
+  //process.exit(0);
 });
